2802a09e14738ef5fe3a2e1b1b7f96b81a460f9f
AdolfoCunquero <adolfocunquero.077@gmail.com>
2021-08-22 02:58:20 -0600

    Nuevo campo rol

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -27,7 +27,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id","email","username","first_name","last_name"]
+        fields = ["id","email","username","first_name","last_name", "rol"]
 
 
 class UserUpdateSerialier(serializers.ModelSerializer):
