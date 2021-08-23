4fa89c2eedb97aff83c134a2bf27872b834db27e
AdolfoCunquero <adolfocunquero.077@gmail.com>
2021-08-23 13:05:09 -0600

    Prueba static fils

--- api_control_asistencia/settings.py
+++ api_control_asistencia/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import datetime
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,6 +147,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = './static/'
+#STATIC_ROOT = './static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'user.User'
