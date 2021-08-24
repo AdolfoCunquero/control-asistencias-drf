d0f14325314d2c311ac0f19270c8e93e88cd84d2
AdolfoCunquero <adolfocunquero.077@gmail.com>
2021-08-24 12:06:04 -0600

    Cambio a BD Mysql

--- api_control_asistencia/settings.py
+++ api_control_asistencia/settings.py
@@ -84,12 +84,23 @@ WSGI_APPLICATION = 'api_control_asistencia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'DBControlAsistenciaUMG',
+            'USER': 'usr_control_asistencia',
+            'PASSWORD': 'C0ntr0l@515t3nc1@',
+            'HOST': 'proyecto2.cbfae7gyoyeq.us-east-2.rds.amazonaws.com',
+            'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
